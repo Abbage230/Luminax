13308fb700e1311c7dfeca79bdc1dc356ec69398
Satherov <satherov@users.noreply.github.com>
2026-05-10 21:15:48 +0200

    Initial 26.1 Commit

---
+++ src/main/java/dev/satherov/luminax/Luminax.java
@@ -0,0 +1,15 @@
+package dev.satherov.luminax;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLModContainer;
+
+@Mod(Luminax.MOD_ID)
+public class Luminax {
+    
+    public static final String MOD_ID = "luminax";
+    
+    public Luminax(final IEventBus bus, final FMLModContainer container) {
+        
+    }
+}
---
+++ src/main/java/dev/satherov/luminax/LuminaxClient.java
@@ -0,0 +1,16 @@
+package dev.satherov.luminax;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLModContainer;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = Luminax.MOD_ID, dist = Dist.CLIENT)
+public class LuminaxClient {
+    
+    public LuminaxClient(final IEventBus bus, final FMLModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+}
